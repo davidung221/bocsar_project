c2606d96f0a22c3c7969223686f04ef0d4cc1889
davidung221 <davidung221@outlook.com>
2026-09-05 17:43:16 +1000

    Update yoy_pct_change.sql

--- sql_queries/yoy_pct_change.sql
+++ sql_queries/yoy_pct_change.sql
@@ -1,4 +1,17 @@
---YoY% change
+/* 
+==============================================================================================
+This query shows the year-over-year % change in incidents per year, tracking change over time.
+==============================================================================================
+1. First CTE 'yearly_totals' gets the total incidents per year.
+2. Second CTE 'comparison' uses LAG() to get the previous year's total.
+3. Final SELECT statement calculates percentage change rounded to 1 decimal point.
+
+E.g.,
+YEAR | TOTAL_INCIDENTS | PREVIOUS_YEAR_TOTAL | PCT_CHANGE
+1996 | 601692 | 528888 | 13.8
+==============================================================================================
+*/
+
 WITH yearly_totals AS (
     SELECT
         EXTRACT(YEAR FROM month) AS year,
