e5e306e56d74935decdd4e56b6d2d427a9eefdfd
davidung221 <davidung221@outlook.com>
2026-09-05 17:05:46 +1000

    Update lowest_and_highest_incident_count_by_year.sql

--- sql_queries/lowest_and_highest_incident_count_by_year.sql
+++ sql_queries/lowest_and_highest_incident_count_by_year.sql
@@ -1,3 +1,19 @@
+/* 
+==============================================================================================
+This query shows the year in which the lowest and highest number of incidents occurred.
+==============================================================================================
+1. Used a CTE to return the total incidents per year, filtering out 2026 as it only
+contains data for 3 months.
+2. Created a column 'label' for a wide table containing the lowest and highest counts.
+3. Used UNION ALL to combine two SELECT statements. ORDER BY ASC/DESC LIMIT 1 returns only
+the row containing the lowest/highest value for total incidents.
+
+E.g.,
+LABEL | YEAR | TOTAL_INCIDENTS
+lowest | 1995 | 528,888
+==============================================================================================
+*/
+
 WITH cte AS (
    SELECT
        EXTRACT(YEAR FROM month) AS year,
